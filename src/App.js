6a614a4414a875cd30534ce0115f70e427a658ea
zhiai <zhiai@qq.com>
2019-07-14 21:30:32 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React, { Component } from 'react'
-
+import React, {Component} from 'react'
+//import {Button, message} from 'antd'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Admin from './pages/admin/admin'
+import Login from './pages/login/login'
+/*
+应用根组件
+ */
 export default class App extends Component {
-    render() {
-        return <h3>哈哈哈</h3>
-    }
+
+  render() {
+    return (
+      <BrowserRouter>
+      <Switch>
+      <Route path='/login' component={Login}/>
+      <Route path='/admin' component={Admin}/>
+      </Switch>
+      </BrowserRouter>
+    )
+  }
 }
+
+
