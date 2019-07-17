ec6c7bf7fd9089732fcca7f0f3e5ff53d1725438
zhiai <zhiai@qq.com>
2019-07-17 13:46:28 +0800

    header layout

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import {Modal} from 'antd'
 import './index.less'
 export default class Header extends Component {
+    logout=()=>{
+        Modal.confirm()
+    }
     render() {
         return (
             <div className='header'>
                 <div className='header-top'>
-                hello! admin
+                hello! admin &nbsp; &nbsp;
+                <a href="#1" onClick={this.logout}>退出</a>
                 </div>
                 <div className='header-bottom'>
                     <div className='header-bottom-left'>用户</div>
