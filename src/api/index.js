8a301ca522a069cd327c746a1c9cc010f8508017
zhiai <zhiai@qq.com>
2019-07-17 19:40:29 +0800

    catagory

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,6 @@
+import jsonp from 'jsonp'
 import ajax from './ajax';
+import {message} from 'antd'
 export function reqLogin(username,password){
     const BASE=''
    return ajax(
@@ -12,4 +14,18 @@ export function reqLogin(username,password){
         }
     )
 }
+//发送jsonp请求得到天气信息
+export const reqWeather=(city)=>{
+    return new Promise((resolve,reject)=>{
+        const url=`http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+        jsonp(url,{},(error,data)=>{
+            if(!error&& data.error===0){
+                const {dayPictureUrl, weather}=data.results[0].weather_data[0];
+                resolve({dayPictureUrl, weather})
+            }else{
+                message.error('获取天气信息失败')
+            }
+        })
 
+    })
+}
--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
+import { Card, Button, Icon, Table } from 'antd'
+import LinkButton from '../../components/link-button'
 /**
  * 分类管理
  */
 export default class Category extends Component {
+  state={categorys:[]}
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '分类的名称',
+        dataIndex: 'name',
+      },
+      {
+        title: '操作',
+        width:300,
+        render: (category)=><LinkButton>修改分类</LinkButton>
+      },
+    ]
+  }
+  componentWillMount(){
+    this.initColumns();
+  }
   render() {
+    const {categorys}=this.state
+    const extra = (
+      <Button type='primary'>
+        <Icon type='plus' />
+        添加
+      </Button>
+    )
     return (
       <div>
-        Category
+        <Card extra={extra} >
+          <Table
+            columns={this.columns}
+            dataSource={categorys}
+            bordered={true}
+            pagination={{defaultPageSize:3,showQuickJumper:true}}
+          />
+        </Card>
       </div>
     )
   }
