f4bcad7ae3ea2ff9565e83dc183fefb052446aa9
zhiai <zhiai@qq.com>
2019-07-18 10:30:21 +0800

    category complete

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Card, Button, Icon, Table, message, Modal } from 'antd'
 import LinkButton from '../../components/link-button'
-import { reqCategorys } from '../../api'
+import { reqCategorys, reqAddCategory, reqUpdateCategory } from '../../api'
 import AddUpdateForm from './add-update-form'
 /**
  * 分类管理
@@ -10,7 +10,7 @@ export default class Category extends Component {
   state = {
     loading: false,
     categorys: [],
-    showStatus:0//改数值定义模态框0为隐藏1为显示添加2为显示修改
+    showStatus: 0//改数值定义模态框0为隐藏1为显示添加2为显示修改
   }
   initColumns = () => {
     //const {showStatus}=this.state
@@ -22,7 +22,12 @@ export default class Category extends Component {
       {
         title: '操作',
         width: 300,
-        render: (category) => <LinkButton onClick={()=>{this.setState({showStatus:2})}}>修改分类</LinkButton>
+        render: (category) => <LinkButton
+          onClick={() => {
+            this.category = category
+            this.setState({ showStatus: 2 })
+          }}
+        >修改分类</LinkButton>
       },
     ]
   }
@@ -41,14 +46,36 @@ export default class Category extends Component {
     }
   }
   //点击模态框中的确定去添加和修改分类
-  handleOk=()=>{
-    this.form.validateFields((err,value)=>{
-
+  handleOk = () => {
+    this.form.validateFields(async (err, value) => {
+      if (!err) {
+        const { categoryName } = value
+        const { showStatus } = this.state
+        let result
+        if (showStatus === 1) {//发送添加分类的请求
+          result = await reqAddCategory(categoryName)
+        } else {//发送修改分类的请求
+          const categoryId = this.category._id
+          result = await reqUpdateCategory({ categoryId, categoryName })
+        }
+        this.setState({ showStatus: 0 })
+        this.form.resetFields()//重置输入数据为初始值，不然initialValue始终以你设置的最新值为默认值会引起冲突
+        const action = showStatus === 1 ? '添加' : '修改'
+        if (result.status === 0) {
+          this.getCategorys()
+          message.success(action + '分类成功')
+        } else {
+          message.error(action + '分类失败')
+        }
+      }
     })
   }
   //点击取消模态框消失
-  handleCancel=()=>{
-    this.setState({showStatus:0})
+  handleCancel = () => {
+    this.form.resetFields()
+    this.setState({
+      showStatus: 0
+    })
   }
   componentWillMount() {
     this.initColumns()
@@ -57,9 +84,10 @@ export default class Category extends Component {
     this.getCategorys()
   }
   render() {
-    const { categorys, loading,showStatus } = this.state
+    const { categorys, loading, showStatus } = this.state
+    const category = this.category || '{}'
     const extra = (
-      <Button type='primary' onClick={()=>{this.setState({showStatus:1})}}>
+      <Button type='primary' onClick={() => { this.setState({ showStatus: 1 }) }}>
         <Icon type='plus' />
         添加
       </Button>
@@ -76,12 +104,12 @@ export default class Category extends Component {
             pagination={{ defaultPageSize: 3, showQuickJumper: true }}
           />
           <Modal
-            title={showStatus===1?"添加分类":"修改分类"}
-            visible={showStatus!==0}
+            title={showStatus === 1 ? "添加分类" : "修改分类"}
+            visible={showStatus !== 0}
             onOk={this.handleOk}
             onCancel={this.handleCancel}
           >
-            <AddUpdateForm setForm={(form)=>this.form=form} categoryName={categorys.name}/>
+            <AddUpdateForm setForm={(form) => this.form = form} categoryName={category.name} />
           </Modal>
         </Card>
       </div>
